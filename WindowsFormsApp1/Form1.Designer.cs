aaaaa2bbf69691088dfb3f7058aa804bc26195cf
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-06-08 20:39:01 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(67, 25);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(657, 396);
+            this.treeView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string folderImagePath = "folder.png";
+            string fileImagePath = "file.png";
+            ImageList imageList = new ImageList();
+            imageList.Images.Add("folder", Image.FromFile(folderImagePath));
+            imageList.Images.Add("file", Image.FromFile(fileImagePath));
+            treeView1.ImageList = imageList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string rootPath = @"C:\Program Files (Arm)\";
+
+            TreeNode rootNode = new TreeNode(rootPath, 0, 0);
+            treeView1.Nodes.Add(rootNode);
+
+            LoadDirectory(rootNode, rootPath);
+        }
+
+        private void LoadDirectory(TreeNode parentNode, string path)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            foreach (DirectoryInfo dir in directoryInfo.GetDirectories())
+            {
+                TreeNode directoryNode = new TreeNode(dir.Name, 0, 0);
+                parentNode.Nodes.Add(directoryNode);
+
+                LoadDirectory(directoryNode, dir.FullName);
+            }
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                TreeNode fileNode = new TreeNode(file.Name, 1, 1);
+                parentNode.Nodes.Add(fileNode);
+            }
+        }
+    }
+}
